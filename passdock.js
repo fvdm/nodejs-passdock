dec07cb47e7097de7c36cc1fe0b4e41f3afbc2fe
Franklin <fr@nkl.in>
2025-04-05 21:45:01 +0200

    Refactor: Modernized syntax
    Badly needed an eye cleaner

--- passdock.js
+++ passdock.js
@@ -2,154 +2,129 @@
 Name      : passdock
 Source    : https://github.com/fvdm/nodejs-passdock
 Feedback  : https://github.com/fvdm/nodejs-passdock/issues
-License   : Unlicense / Public Domain
-
-This is free and unencumbered software released into the public domain.
-
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to <http://unlicense.org/>
+License   : Unlicense (Public Domain, see LICENSE file)
 */
 
-var https = require ('https')
-var querystring = require ('querystring')
+const { request } = require( 'https' );
+const { stringify } = require( 'querystring' );
 
 var app = {
   api: {
     host: 'api.passdock.com',
     path: '/api/v1/',
     timeout: 30,
-    token: ''
-  }
-}
+    token: '',
+  },
+};
 
 // Communication error
-app.buildError = function ( reason, response, requestBody ) {
+app.buildError = ( reason, response, requestBody ) => {
   return {
     reason: reason,
     response: {
       headers: response.headers,
       statusCode: response.statusCode,
-      complete: response.complete
+      complete: response.complete,
     },
     request: {
       method: response.req.method,
       path: response.req.path,
       headers: response.req._headers,
       body: requestBody,
-      finished: response.req.finished
-    }
-  }
-}
+      finished: response.req.finished,
+    },
+  };
+};
 
 // Communicate
 app.talk = function ( method, path, fields, cb ) {
-  if ( !cb && typeof fields == 'function' ){
-    var cb = fields
-    var fields = {}
+  if ( !cb && typeof fields === 'function' ) {
+    let cb = fields;
+    let fields = {};
   }
 	
   // build request
-  fields.api_token = app.api.token
-  fields = querystring.stringify ( fields )
-	
-  var options = {
+  fields.api_token = app.api.token;
+  fields = stringify( fields );
+
+  const options = {
     host: app.api.host,
     port: 443,
-    path: app.api.path + path + (method == 'GET' ? '?' + fields : ''),
+    path: app.api.path + path + ( method === 'GET' ? '?' + fields : '' ),
     method: method,
     agent: false,
     headers: {
       Accept: 'application/json',
-      'User-Agent': 'passdock.js (https://github.com/fvdm/nodejs-passdock)'
-    }
-  }
+      'User-Agent': 'fvdm/nodejs-passdock',
+    },
+  };
 	
-  if ( method != 'GET' ){
-    options.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-    options.headers['Content-Length'] = fields.length
+  if ( method !== 'GET' ) {
+    options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    options.headers['Content-Length'] = fields.length;
   }
 	
   // do request
-  var req = https.request ( options, function ( response ) {
-    var data = ''
-    response.on ( 'data', function ( d ) { data += d })
+  const req = request( options, response => {
+    let data = '';
+    response.on( 'data', d => { data += d; } );
 		
     // process response
-    response.on ( 'end', function () {
-      data = data.toString ('utf8').trim ()
+    response.on( 'end', () => {
+      data = data.toString( 'utf8' ).trim();
 			
-      if ( response.statusCode >= 500 ){
+      if ( response.statusCode >= 500 ) {
 				
         // server trouble
-        cb ( data, app.buildError ( 'server error', response, fields ))
+        cb( data, app.buildError( 'server error', response, fields ) );
 				
-      } else if ( response.statusCode >= 200 && response.statusCode < 300 ){
+      } else if ( response.statusCode >= 200 && response.statusCode < 300 ) {
 				
         // all good
-        if ( data.match ( /^(\{.*\}|\[.*\])$/ ) ){
-          cb ( JSON.parse ( data ), false )
+        if ( data.match( /^(\{.*\}|\[.*\])$/ ) ) {
+          cb( JSON.parse( data ), false );
         } else {
-          cb ( data, app.buildError ( 'invalid data', response, fields ))
+          cb( data, app.buildError( 'invalid data', response, fields ) );
         }
 				
       } else {
 				
         // API error
-        cb ( data, app.buildError ( 'error', response, fields ))
+        cb( data, app.buildError( 'error', response, fields ) );
 				
       }
-    })
+    } );
 		
     // request cut off
-    response.on ( 'close', function ( err ) {
-      cb ( data.toString ('utf8'), app.buildError ( 'early disconnect', response, fields ))
-    })
-  })
+    response.on( 'close', err => {
+      cb( data.toString( 'utf8' ), app.buildError( 'early disconnect', response, fields ) );
+    } );
+  } );
 	
-  req.setTimeout ( app.api.timeout * 1000 )
+  req.setTimeout( app.api.timeout * 1000 );
 	
-  if ( method != 'GET' ){
-    req.write ( fields )
+  if ( method !== 'GET' ) {
+    req.write( fields );
   }
 	
-  req.end ()
+  req.end();
 }
 
 // Templates
 app.templates = {
-  list: function ( cb ) { app.talk ( 'GET', 'templates', cb ) },
-  show: function ( id, cb ) { app.talk ( 'GET', 'templates/' + id, cb ) },
-  delete: function ( id, cb ) { app.talk ( 'DELETE', 'templates/' + id, cb ) }
-}
+  list: ( cb ) => app.talk( 'GET', 'templates', cb ),
+  show: ( id, cb ) => app.talk( 'GET', `templates/${id}`, cb ),
+  delete: ( id, cb ) => app.talk( 'DELETE', `templates/${id}`, cb ),
+};
 
 // Passes
 app.passes = {
-  list: function ( tid, cb ) { app.talk ( 'GET', 'templates/' + tid +'/passes', cb ) },
-  show: function ( tid, id, cb ) { app.talk ( 'GET', 'templates/' + tid +'/passes/' + id, cb ) },
-  delete: function ( tid, id, cb ) { app.talk ( 'DELETE', 'templates/' + tid +'/passes/' + id, cb ) },
-  create: function ( tid, pass, cb ) { app.talk ( 'POST', 'templates/' + tid +'/passes', { pass: pass }, cb ) },
-  update: function ( tid, pid, pass, cb ) { app.talk ( 'PUT', 'templates/' + tid +'/passes/' + pid, { pass: pass }, cb ) }
-}
+  list: ( tid, cb ) => app.talk( 'GET', `templates/${tid}/passes`, cb ),
+  show: ( tid, id, cb ) => app.talk( 'GET', `templates/${tid}/passes/${id}`, cb ),
+  delete: ( tid, id, cb ) => app.talk( 'DELETE', `templates/${tid}/passes/${id}`, cb ),
+  create: ( tid, pass, cb ) => app.talk( 'POST', `templates/${tid}/passes`, { pass }, cb ),
+  update: ( tid, pid, pass, cb ) => app.talk( 'PUT', `templates/${tid}/passes/${pid}`, { pass }, cb ),
+};
 
 // module magic
-module.exports = app
+module.exports = app;
